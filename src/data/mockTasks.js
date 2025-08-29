778ccd2d5a64b8c12c12915d131d1d94b7b225ed
fangyi9430 <fangyili1998@gmail.com>
2025-08-29 12:48:19 -0400

    set up environment and mock data.

---
+++ src/data/mockTasks.js
@@ -0,0 +1,20 @@
+export const mockTasks = [
+  {
+    id: 1,
+    title: "finish report.",
+    description: "friday.",
+    status: "pending"
+  },
+  {
+    id: 2,
+    title: "React",
+    description: "finish React",
+    status: "completed"
+  },
+  {
+    id: 3,
+    title: "grocery.",
+    description: "milk.",
+    status: "pending"
+  }
+];
